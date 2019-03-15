c598ecb68fcda02db7391d79e6312d27546748a7
Huang Xingqiao <a92304a92304@gmail.com>
2019-03-15 13:27:18 +0800

    add Card.swift & MatchGame.swift

---
+++ CardGame01/Card.swift
@@ -0,0 +1,13 @@
+//
+//  Card.swift
+//  CardGame01
+//
+//  Created by Sean on 2019/3/15.
+//  Copyright © 2019 Sean. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+  
+}
---
+++ CardGame01/MatchingGame.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+class MatchingCard {
+    var cards: Array<Card>
+    func chooseCard(at index: Int) {
+    
+    }
+}
