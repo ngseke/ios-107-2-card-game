54e26a80eae3f46b7d430a296bcce0284e2057a5
Huang Xingqiao <a92304a92304@gmail.com>
2019-03-15 13:22:36 +0800

    返回舊版本。

--- CardGame01/ViewController.swift
+++ CardGame01/ViewController.swift
@@ -8,10 +8,8 @@ class ViewController: UIViewController {
     }
     
     @IBOutlet weak var flipCountLabel: UILabel!
-    @IBOutlet var cardButton: [UIButton]!
     
-    var emojis = ["👻", "💩", "🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🦝", "🐻", "🐼",  "🦘",  "🦡",  "🐨", "🐯", "🐤"]
-    
+    var emojis = ["👻", "💩"]
     
     var flipCount:Int = 0
     {
@@ -19,35 +17,22 @@ class ViewController: UIViewController {
             flipCountLabel.text = "Flips: \(flipCount)"
         }
     }
-
-    @IBAction func reset(_ sender: UIButton) {
-        for (index, i) in cardButton.enumerated() {
-            flipcard(withEmoji: "", on: i, isAll: true)
-        }
-        flipCount = 0
-        emojis.shuffle()
-    }
     
-    @IBAction func flipAll(_ sender: UIButton) {
-        for (index, i) in cardButton.enumerated() {
-            flipcard(withEmoji: emojis[index], on: i, isAll: true)
-        }
-
-    }
+    
     
     @IBAction func touchCard(_ sender: UIButton) {
-        if let cardNumber = cardButton.index(of: sender) {
-            flipcard(withEmoji: emojis[cardNumber], on: sender)
-        } else {
-            print("oops")
-        }
-        
+        flipcard(withEmoji: "👻", on: sender)
+        flipCount += 1
+    }
+    
+    @IBAction func touchCardPoop(_ sender: UIButton) {
+        flipcard(withEmoji: "💩", on: sender)
         flipCount += 1
     }
     
     // 令呼叫函式像是在讀英文
-    func flipcard(withEmoji emoji: String, on button: UIButton, isAll: Bool = false) {
-        if (button.currentTitle == emoji && !isAll) || emoji == "" {
+    func flipcard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
             button.setTitle("", for: UIControl.State.normal)
             button.backgroundColor = #colorLiteral(red: 0.9411764741, green: 0.760784328, blue: 0.1490196139, alpha: 1)
         } else {
